c6d36f133dcac621d3c190f50995c6696e88ebaa
root <root@ip-11-0-30-18.ap-south-1.compute.internal>
2024-08-07 10:37:47 +0000

    adding install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+sudo yum update -y
+sudo yum install httpd -y
+sudo yum install java -y 
+sudo yum install wget -y
+sudo yum install git -y
+sudo yum install net-tools -y 
+sudo yum install tree -y
