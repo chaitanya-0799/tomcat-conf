956db3ba8f08d8e059eeab52f1e9b9990718496a
chaitanya-0799 <146313463+chaitanya-0799@users.noreply.github.com>
2025-02-28 20:31:43 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,18 @@
 #! /bin/bash
 
-sudo yum update -y
-sudo yum install httpd -y
-sudo yum install java -y 
-sudo yum install wget -y
-sudo yum install git -y
-sudo yum install net-tools -y 
-sudo yum install tree -y
+set -e
+
+if [ $USER = "ec2-user" ]; then
+    echo "Installing the packages"
+    sudo yum update -y
+    sudo yum install httpd -y
+    sudo yum install java -y
+    sudo yum install wget -y
+    sudo yum install git -y
+    sudo yum install net-tools -y
+    sudo yum install tree -y
+
+else
+    echo "THESE PACKAGES CAN ONLY BE INSTALLED BY THE EC2-USER"
+
+fi
