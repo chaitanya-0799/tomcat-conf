cf8b5ef613b36867f1adf5f33d57fb2efc10d3f8
chaitanya-0799 <146313463+chaitanya-0799@users.noreply.github.com>
2025-02-28 20:09:46 +0530

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -1,21 +1,55 @@
 #! /bin/bash
 
-wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.91/bin/apache-tomcat-9.0.91.tar.gz
+set -e
 
-sudo tar -xvzf apache-tomcat-9.0.91.tar.gz
-sudo rm -rf apache-tomcat-9.0.91.tar.gz
+echo "#########################################################"
+echo "##                                                     ##"
+echo "##               INSTALLING TOMCAT                     ##"
+echo "##                                                     ##"
+echo "#########################################################"
 
-sudo rm -rf apache-tomcat-9.0.91/conf/tomcat-users.xml
-sudo rm -rf apache-tomcat-9.0.91/webapps/host-manager/META-INF/context.xml
-sudo rm -rf apache-tomcat-9.0.91/webapps/manager/META-INF/context.xml
+echo "Cleaning up previous installations"
 
-sudo chmod 640 /home/ec2-user/context.xml
-sudo chmod 600 /home/ec2-user/tomcat-users.xml
+sudo rm -rf /opt/tomcat apache-tomcat-9.0.100.tar.gz apache-tomcat-9.0.100
 
-sudo chown root:root /home/ec2-user/context.xml
-sudo chown root:root /home/ec2-user/tomcat-users.xml
+echo "Downloading Tomcat 9.0.100"
+
+wget -q https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.100/bin/apache-tomcat-9.0.100.tar.gz 
 
 
-sudo cp /home/ec2-user/tomcat-users.xml  /home/ec2-user/apache-tomcat-9.0.91/conf/
-sudo cp /home/ec2-user/context.xml  /home/ec2-user/apache-tomcat-9.0.91/webapps/host-manager/META-INF/
-sudo cp /home/ec2-user/context.xml  /home/ec2-user/apache-tomcat-9.0.91/webapps/manager/META-INF/
+
+sudo tar -xzf apache-tomcat-9.0.100.tar.gz
+sudo rm -rf apache-tomcat-9.0.100.tar.gz
+
+sudo chown root:root context.xml
+sudo chown root:root tomcat-users.xml
+
+sudo chmod 640 context.xml
+sudo chmod 600 tomcat-users.xml
+
+sudo mv apache-tomcat-9.0.100 /opt/tomcat
+
+sudo rm -rf /opt/tomcat/conf/tomcat-users.xml
+sudo rm -rf /opt/tomcat/webapps/host-manager/META-INF/context.xml
+sudo rm -rf /opt/tomcat/webapps/manager/META-INF/context.xml
+
+sudo cp tomcat-users.xml /opt/tomcat/conf/
+sudo cp context.xml /opt/tomcat/webapps/host-manager/META-INF/
+sudo cp context.xml /opt/tomcat/webapps/manager/META-INF/
+
+sudo sh /opt/tomcat/bin/startup.sh
+
+
+if [ $? -eq 0 ]; then
+    echo "       "
+    echo "Tomcat has been installed successfully"
+    echo "       "
+    echo "*************************************************************"
+    echo "      "
+    echo "ACCESS TOMCAT ON http://$(curl -s ifconfig.me):8080"
+    echo "      "
+    echo "*************************************************************"
+
+else
+    echo "Tomcat installation failed"
+fi
